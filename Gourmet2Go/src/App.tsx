aae85b98ab73b2a8d335ac6a1473f72c2793685a
Caleb <calebtarantini@gmail.com>
2026-02-01 15:44:41 -0500

    Made change to 'iclude' statement in tsconfig.app.json as I was getting an error on an import. changed app to only add functioning pages I just completed

--- Gourmet2Go/src/App.tsx
+++ Gourmet2Go/src/App.tsx
@@ -1,24 +1,13 @@
 import { Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { Footer } from "./components/Footer";
+import { AdminMenuPage } from "./pages/AdminMenuPage";
+import { AdminAddMenuPage } from "./pages/AdminAddMenuPage";
+import { AdminEditMenuPage } from "./pages/AdminEditMenuPage";
 import { SignUpPage } from "./pages/SignUpPage";
 import { SignInPage } from "./pages/SignInPage";
 import { NotFoundPage } from "./pages/NotFoundPage";
 import { ToSPage } from "./pages/ToSPage";
-import { PrivacyPolicyPage } from "./pages/PrivacyPolicyPage";
-import { Navbar } from "./components/Navbar";
-import { Footer } from "./components/Footer";
-import { AdminHomePage } from "./pages/AdminHomePage";
-import { AdminMenuPage } from "./pages/AdminMenuPage";
-import { AdminAddMenuPage } from "./pages/AdminAddMenuPage";
-import { AdminEditMenuPage } from "./pages/AdminEditMenuPage";
-import { AdminEditMenuItemPage } from "./pages/AdminEditMenuItemPage";
-import { AdminNewMenuItemPage } from "./pages/AdminNewMenuItemPage";
-import { AdminOrdersPage } from "./pages/AdminOrdersPage";
-// import { HomePage } from "./pages/HomePage";
-// import { Navbar } from "./components/Navbar";
-// import { AddMessagePage } from "./pages/AddMessagePage";
-// import { UpdateMessagePage } from "./pages/UpdateMessagePage";
-// import { UserListPage } from "./pages/UserListPage";
-// import { SecretPage } from "./pages/SecretPage";
 
 function App() {
   return (
@@ -29,18 +18,12 @@ function App() {
         {/*All the routes are defined here*/}
           <Routes>
             <Route path="/sign-up" element={<SignUpPage />} />
-            <Route path="/sign-in" element={<SignInPage />} />
-            <Route path="/" element={<p>Welcome to Gourmet2Go!</p>} />
+            <Route path="/sign-in" element={<SignInPage />} /> 
+            <Route path="/menu" element={<AdminMenuPage />} />       
             <Route path="*" element={<NotFoundPage />} />
             <Route path="/terms-of-service" element={<ToSPage />} />
-            <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
-            <Route path="/home" element={<AdminHomePage />} />
-            <Route path="/menu" element={<AdminMenuPage />} />
             <Route path="/menu/add/:date" element={<AdminAddMenuPage />} />
             <Route path="/menu/edit/:date" element={<AdminEditMenuPage />} />
-            <Route path="/menu/item/edit/:name" element={<AdminEditMenuItemPage />} />
-            <Route path="/menu/item/new" element={<AdminNewMenuItemPage />} />
-            <Route path ="/orders" element={<AdminOrdersPage />} />
           </Routes>
       </div>
       <Footer />
