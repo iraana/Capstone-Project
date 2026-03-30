ae194ef8866273e702d9c1b41da9b51eff02f796
iraana <24048851@saultcollege.ca>
2026-03-29 23:20:57 -0400

    fix some pages ui bugs and added trailer/screenshots to README

--- src/pages/UserOrdersPage.tsx
+++ src/pages/UserOrdersPage.tsx
@@ -2,7 +2,7 @@ import { UserOrders } from "../components/UserOrders"
 
 export const UserOrdersPage = () => {
     return (
-        <main className="min-h-screen bg-gray-50 dark:bg-zinc-900 -mt-20">
+        <main className="bg-white dark:bg-zinc-900 px-4 sm:px-6 lg:px-8 py-10 transition-colors">
             <UserOrders />
         </main>
     )
